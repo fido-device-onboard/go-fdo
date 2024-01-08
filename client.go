92cec1202cbcd2c82fbe801bf3fdf78bafd2738a
Ben Krieger <ben.krieger@intel.com>
2024-01-08 15:50:04 -0500

    Move transport interface and protocol enum to a separate file to prepare for Client method implementation

--- client.go
+++ client.go
@@ -5,39 +5,8 @@ package fdo
 
 import (
 	"context"
-	"io"
 	"time"
 )
-
-// Protocol is the FDO specification-defined protocol.
-type Protocol uint8
-
-const (
-	UnknownProtocol Protocol = iota
-	DIProtocol
-	TO0Protocol
-	TO1Protocol
-	TO2Protocol
-	AnyProtocol // for error message types
-)
-
-// ProtocolOf returns the protocol a given message type belongs to.
-func ProtocolOf(msgType uint8) Protocol {
-	switch msgType {
-	case 10, 11, 12, 13:
-		return DIProtocol
-	case 20, 21, 22, 23:
-		return TO0Protocol
-	case 30, 31, 32, 33:
-		return TO1Protocol
-	case 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71:
-		return TO2Protocol
-	case 255:
-		return AnyProtocol
-	default:
-		return UnknownProtocol
-	}
-}
 
 // Client implements methods for performing FDO protocols DI (non-normative),
 // TO1, and TO2.
@@ -76,11 +45,6 @@ func (*Client) TransferOwnership1(ctx context.Context) error {
 // FSIMs.
 func (*Client) TransferOwnership2(ctx context.Context) error {
 	panic("unimplemented")
-}
-
-// Transport abstracts the underlying TCP/HTTP/CoAP transport.
-type Transport interface {
-	Send(ctx context.Context, base string, msgType uint8, msg any) (respType uint8, _ io.ReadCloser, _ error)
 }
 
 // RetryDecider allows for deciding whether a retry should occur, based on the
