1ceb1af73b91011fffabff9a47e604e20de448f1
Ben Krieger <ben.krieger@intel.com>
2025-01-16 13:57:38 -0500

    tpm: Allow devices with indexes greater than 0, e.g. /dev/tpmrm1
    Signed-off-by: Ben Krieger <ben.krieger@intel.com>

---
+++ tpm/alias_test.go
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache 2.0
+
+package tpm
+
+// IsDevNode aliases the private function for testing.
+var IsDevNode = isDevNode
